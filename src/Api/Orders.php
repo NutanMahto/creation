a997e823b954f0ffdbc0ab6e374212fdcb15810c
NutanMcc <nutan@multichannelcreative.co.uk>
2020-11-26 14:05:49 +0530

    orders update

--- src/Api/Orders.php
+++ src/Api/Orders.php
@@ -5,6 +5,32 @@ namespace Linn\Creation\Api;
 class Orders extends ApiClient
 {
 
+
+
+    public function createOrders($orders){
+
+        // remove null
+        $orders = array_filter($orders, function($row){
+            return ! is_null($row);
+        });
+
+        return $this->post('Orders/CreateOrders',[
+            'location' => 'default',
+            'orders' => json_encode($orders)
+        ]);
+    }
+
+
+    public function cancelOrder( $orderId,  $fulfilmentCenter,  $refund = 0,  $note = "" )
+    {
+        return $this->post('Orders/CancelOrder',[
+            'orderId' => $orderId,
+            'fulfilmentCenter' => $fulfilmentCenter,
+            'refund' => $refund,
+            'note' => $note,
+        ]);
+    }
+
     public function getOpenOrders( $fulfilmentCenter,  $entriesPerPage = 25,  $pageNumber = 1,  $filters = "",  $sorting = [],  $additionalFilters = "")
     {
         return $this->get('Orders/GetOpenOrders', [
@@ -111,166 +137,5 @@ class Orders extends ApiClient
         ]);
     }
 
-    public function createOrder(
-         $orderItems,
-         $locationName = "",
-         $matchPostalServiceTag = "",
-         $source = "",
-         $subSource = "",
-         $referenceNumber = "",
-         $externalReference = "",
-         $receivedDate = "",
-         $dispatchBy = "",
-         $currency = "",
-         $channelBuyerName = "",
-         $deliveryEmailAddress = "",
-         $deliveryAddress1 = "",
-         $deliveryAddress2 = "",
-         $deliveryAddress3 = "",
-         $deliveryTown = "",
-         $deliveryRegion = "",
-         $deliveryPostCode = "",
-         $deliveryCountryName = "",
-         $deliveryFullName = "",
-         $deliveryCompanyName = "",
-         $deliveryPhoneNumber = "",
-         $deliveryIso3CountryCode = "",
-         $billingAddress1 = "",
-         $billingAddress2 = "",
-         $billingAddress3 = "",
-         $billingTown = "",
-         $billingRegion = "",
-         $billingPostCode = "",
-         $billingCountryName = "",
-         $billingFullName = "",
-         $billingCompanyName = "",
-         $billingPhoneNumber = "",
-         $billingIso3CountryCode = "",
-         $shippingCost = 0,
-         $shippingTaxRate = 20,
-         $mappingSource = "",
-         $orderState = "None",
-         $paymentStatus = "Paid",
-         $paidDate = "Today"
-    ){
-        $order = [
-            //minimum requirements
-            "Source" => $source,
-            "SubSource" => $subSource,
-            "ReferenceNumber" => $referenceNumber,
-            "ExternalReference" => $externalReference,
-            "ReceivedDate" => date('c', strtotime($receivedDate)),
-            "DispatchBy" => date('c', strtotime($dispatchBy)),
-//            "DeliveryStartDate" => date('c', strtotime('15th January 2018')),
-//            "DeliveryEndDate" => "2019-02-12T11:12:33.4177471+01:00",
-
-            //Basic info
-            "Currency" => $currency, //USD EUR
-            'ChannelBuyerName' => $channelBuyerName,
-            'MatchPostalServiceTag' => $matchPostalServiceTag,
-            'DeliveryAddress' => [
-                'EmailAddress' => $deliveryEmailAddress,
-                'Address1' => $deliveryAddress1,
-                'Address2' => $deliveryAddress2,
-                'Address3' => $deliveryAddress3,
-                'Town' =>  $deliveryTown,
-                'Region' =>  $deliveryRegion,
-                'PostCode' =>  $deliveryPostCode,
-                'Country' =>  $deliveryCountryName,
-                'FullName' =>  $deliveryFullName,
-                'Company' =>  $deliveryCompanyName,
-                'PhoneNumber' =>  $deliveryPhoneNumber,
-                'MatchCountryCode' =>  $deliveryIso3CountryCode
-            ],
-            'BillingAddress' => [
-                'Address1' => $billingAddress1,
-                'Address2' => $billingAddress2,
-                'Address3' => $billingAddress3,
-                'Town' =>  $billingTown,
-                'Region' =>  $billingRegion,
-                'PostCode' =>  $billingPostCode,
-                'Country' =>  $billingCountryName,
-                'FullName' =>  $billingFullName,
-                'Company' =>  $billingCompanyName,
-                'PhoneNumber' =>  $billingPhoneNumber,
-                'MatchCountryCode' =>  $billingIso3CountryCode
-            ],
-
-            // items
-            'AutomaticallyLinkBySKU' => true, //if channel mapping does not work, link by sku
-            'MappingSource' => $mappingSource, //use mapping from another channel for SKU's
-            'OrderItems'=> $orderItems->getOrderItems(),
-            'PostalServiceCost' => $shippingCost, //cost inclusive of cost and after discount
-            'PostalServiceTaxRate' => $shippingTaxRate,
-
-            //state
-            'OrderState' => $orderState,
-            'PaymentStatus' => $paymentStatus,
-            'PaidOn' => $paidDate ? date('c', strtotime($paidDate)) : null
-
-        ];
-
-        // remove null
-        $order = array_filter($order, function($row){
-            return ! is_null($row);
-        });
-
-        return $this->post('Orders/CreateOrders',[
-            'Location' => $locationName,
-            'Orders' => json_encode([$order])
-        ]);
-    }
-
-    public function cancelOrder( $orderId,  $fulfilmentCenter,  $refund = 0,  $note = "" )
-    {
-        return $this->post('Orders/CancelOrder',[
-            'orderId' => $orderId,
-            'fulfilmentCenter' => $fulfilmentCenter,
-            'refund' => $refund,
-            'note' => $note,
-        ]);
-    }
-
-//    public function setGeneralInfo(
-//        $guid
-//    ){
-//        $order = [
-////              "Status" => 1,
-////              "LabelPrinted" => true,
-////              "LabelError" => "sample string 3",
-////              "InvoicePrinted" => true,
-////              "PickListPrinted" => true,
-////              "IsRuleRun" => true,
-////              "Notes" => 7,
-////              "PartShipped" => true,
-////              "Marker" => 64,
-////              "IsParked" => true,
-////              "Identifiers" => null,
-////              "ReferenceNum" => "sample string 10",
-////              "SecondaryReference" => "sample string 11",
-////              "ExternalReferenceNum" => "sample string 12",
-////              "ReceivedDate" => "2018-10-03T11:12:33.5138145+01:00",
-////              "Source" => "sample string 14",
-////              "SubSource" => "sample string 15",
-////              "SiteCode" => "sample string 16",
-////              "HoldOrCancel" => true,
-////              "DespatchByDate" => "2018-10-03T11:12:33.5138145+01:00",
-//              "ScheduledDelivery" => [
-//                  "From" => "2019-02-12T11:12:33.5138145+01:00",
-//                  "To" => "2019-12-12T11:12:33.5138145+01:00"
-//              ],
-//              "HasScheduledDelivery" => true,
-////              "Location" => "201863fe-a0a0-4af3-8956-ac185631f82b",
-////              "NumItems" => 20,
-////              "StockAllocationType" => 0
-//        ];
-//
-//        return $this->post('Orders/SetOrderGeneralInfo',[
-//            'orderId' => $guid,
-//            'info' => json_encode($order),
-//            'wasDraft' => false,
-//        ]);
-//
-//    }
 
 }
